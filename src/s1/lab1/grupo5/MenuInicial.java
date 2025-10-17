a7e418861eb91ac87fa55301df79810e530ebea5
Fernando <fernando@aithn.net>
2025-10-17 10:21:06 -0600

    Interfaz de menu inicial, inicio de sesion y creacion de cuenta

--- src/s1/lab1/grupo5/MenuInicial.java
+++ src/s1/lab1/grupo5/MenuInicial.java
@@ -32,8 +32,97 @@ public class MenuInicial extends javax.swing.JFrame{
         menuInicial.add(btnLogin);
         menuInicial.add(btnNuevaCta);
         
+        JFrame login=new JFrame();
+        JPanel panelLogin=new JPanel(null);
+        login.setSize(600,400);
+        login.setResizable(false);
+        login.setContentPane(panelLogin);
+        login.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         
         
+        JLabel lblCorreo=new JLabel("Ingrese su correo: ");
+        panelLogin.add(lblCorreo);
+        lblCorreo.setFont(font);
+        lblCorreo.setBounds(150,50,300,30);
+        
+        JLabel lblContra=new JLabel("Ingrese su contrasena: ");
+        panelLogin.add(lblContra);
+        lblContra.setFont(font);
+        lblContra.setBounds(150,140,300,30);
+        
+        JTextField fldCorreo=new JTextField();
+        panelLogin.add(fldCorreo);
+        fldCorreo.setBounds(150,85,300,30);
+        
+        JTextField fldContra=new JTextField();
+        panelLogin.add(fldContra);
+        fldContra.setBounds(150,175,300,30);
+        
+        
+        JButton btnAceptar=new JButton("Aceptar");
+        panelLogin.add(btnAceptar);
+        btnAceptar.setFont(font);
+        btnAceptar.setBounds(225,250,150,40);
+        
+        btnAceptar.addActionListener(e->{
+            login.dispose();
+            this.setVisible(true);
+        });
+        
+        JFrame crearCta=new JFrame();
+        JPanel panelCta=new JPanel(null);
+        crearCta.setSize(600,400);
+        crearCta.setResizable(false);
+        crearCta.setContentPane(panelCta);
+        crearCta.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        
+        JLabel lblNombre=new JLabel("Ingrese su nombre: ");
+        panelCta.add(lblNombre);
+        lblNombre.setFont(font);
+        lblNombre.setBounds(150,30,300,30);
+        
+        JTextField fldNombre=new JTextField();
+        panelCta.add(fldNombre);
+        fldNombre.setBounds(150,65,300,30);
+        
+        JLabel lblCorreo2=new JLabel("Ingrese su correo: ");
+        panelCta.add(lblCorreo2);
+        lblCorreo2.setFont(font);
+        lblCorreo2.setBounds(150,120,300,30);
+        
+        JTextField fldCorreo2=new JTextField();
+        panelCta.add(fldCorreo2);
+        fldCorreo2.setBounds(150,155,300,30);
+        
+        JLabel lblContra2=new JLabel("Ingrese su contrasena: ");
+        panelCta.add(lblContra2);
+        lblContra2.setBounds(150,210,300,30);
+        lblContra2.setFont(font);
+        
+        JTextField fldContra2=new JTextField();
+        panelCta.add(fldContra2);
+        fldContra2.setBounds(150,245,300,30);
+        fldContra2.setFont(font);
+        
+        JButton btnAceptar2=new JButton("Aceptar");
+        crearCta.add(btnAceptar2);
+        crearCta.setFont(font);
+        btnAceptar2.setBounds(225,300,150,40);
+        
+        btnAceptar2.addActionListener(e->{
+            crearCta.dispose();
+            this.setVisible(true);
+        });
+        
+        btnLogin.addActionListener(e->{
+            this.setVisible(false);
+            login.setVisible(true);
+        });
+        
+        btnNuevaCta.addActionListener(e->{
+            this.setVisible(false);
+            crearCta.setVisible(true);
+        });
         
         
     }
